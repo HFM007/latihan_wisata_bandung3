ade92287e170282c5e23fc6d5fda75a87d40c5f0
HFM007 <habib.fauz43@gmail.com>
2024-11-17 14:02:35 +0700

    Refactor DetailScreen to accept TourismPlace data and enhance UI with image carousel and favorite button

--- lib/main.dart
+++ lib/main.dart
@@ -31,7 +31,7 @@ class MainScreen extends StatelessWidget {
           return InkWell(
             onTap: () {
               Navigator.push(context, MaterialPageRoute(builder: (context) {
-                return DetailScreen();
+                return DetailScreen(place: place);
               }));
             },
             child: Card(
@@ -72,16 +72,143 @@ class MainScreen extends StatelessWidget {
   }
 }
 
-class DetailScreen extends StatefulWidget {
-  const DetailScreen({super.key});
+class DetailScreen extends StatelessWidget {
+  final TourismPlace place;
+  const DetailScreen({super.key, required this.place});
 
-  @override
-  State<DetailScreen> createState() => _DetailScreenState();
-}
-
-class _DetailScreenState extends State<DetailScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Stack(
+              children: [
+                Image.asset(place.imageAsset),
+                SafeArea(
+                    child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      CircleAvatar(
+                        backgroundColor: Colors.grey,
+                        child: IconButton(
+                          icon: const Icon(
+                            Icons.arrow_back,
+                            color: Colors.white,
+                          ),
+                          onPressed: () {
+                            Navigator.pop(context);
+                          },
+                        ),
+                      ),
+                      const FavoriteButton(),
+                    ],
+                  ),
+                ))
+              ],
+            ),
+            Container(
+              margin: const EdgeInsets.only(top: 16.0),
+              child: Text(
+                place.name,
+                textAlign: TextAlign.center,
+                style: const TextStyle(
+                  fontSize: 30.0,
+                ),
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.symmetric(vertical: 16.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Column(
+                    children: [
+                      const Icon(Icons.calendar_today),
+                      const SizedBox(height: 8.0),
+                      Text(
+                        place.openDays,
+                        style: const TextStyle(fontSize: 16.0),
+                      )
+                    ],
+                  ),
+                  Column(
+                    children: [
+                      const Icon(Icons.monetization_on),
+                      const SizedBox(height: 8.0),
+                      Text(
+                        place.ticketPrice,
+                        style: const TextStyle(fontSize: 16.0),
+                      )
+                    ],
+                  ),
+                  Column(
+                    children: [
+                      const Icon(Icons.access_time),
+                      const SizedBox(height: 8.0),
+                      Text(
+                        place.openTime,
+                        style: const TextStyle(fontSize: 16.0),
+                      )
+                    ],
+                  )
+                ],
+              ),
+            ),
+            Container(
+              padding: const EdgeInsets.all(16.0),
+              child: Text(
+                place.description,
+                textAlign: TextAlign.justify,
+                style: const TextStyle(fontSize: 16.0),
+              ),
+            ),
+            SizedBox(
+                height: 150,
+                child: ListView(
+                  scrollDirection: Axis.horizontal,
+                  children: place.imageUrls.map((url) {
+                    return Padding(
+                      padding: const EdgeInsets.all(4.0),
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(10),
+                        child: Image.network(url),
+                      ),
+                    );
+                  }).toList(),
+                ))
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class FavoriteButton extends StatefulWidget {
+  const FavoriteButton({super.key});
+
+  @override
+  State<FavoriteButton> createState() => _FavoriteButtonState();
+}
+
+class _FavoriteButtonState extends State<FavoriteButton> {
+  bool isFavorite = false;
+  @override
+  Widget build(BuildContext context) {
+    return IconButton(
+      icon: Icon(
+        isFavorite ? Icons.favorite : Icons.favorite_border,
+        color: Colors.red,
+      ),
+      onPressed: () {
+        setState(() {
+          isFavorite = !isFavorite;
+        });
+      },
+    );
   }
 }
